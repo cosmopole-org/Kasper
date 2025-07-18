98d679fde3ffbbe06fb4a30e1faef8a38211289d
keyhan <keyhmoham@gmail.com>
2025-07-18 22:25:19 +0330

    changed runoid image folder structure.

--- applet/runpod/lorax/run.sh
+++ applet/runpod/lorax/run.sh
@@ -1,6 +1,8 @@
 
+cd chain-bridge
 node main.js &
+cd ..
 
 lorax-launcher --json-output --model-id mistralai/Mistral-7B-Instruct-v0.1 --port 80 --max-batch-prefill-tokens 2048 --max-input-length 2048 &
 
-python handler.py
+python runpod-bridge/handler.py
