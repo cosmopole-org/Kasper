f5fcc7d04637259b24d48756bf86b7f539e42124
keyhan <keyhmoham@gmail.com>
2025-07-18 12:33:50 +0330

    fixed bug.

--- applet/runpod/lorax/run.sh
+++ applet/runpod/lorax/run.sh
@@ -1,4 +1,4 @@
 
-lorax-launcher --json-output --model-id mistralai/Mistral-7B-Instruct-v0.1 --port 80 &
+lorax-launcher --json-output --model-id mistralai/Mistral-7B-Instruct-v0.1 --port 80 --max-batch-prefill-tokens 2048 --max-input-length 2048 &
 
 python handler.py
