6f5186c3c56e0ffc728836c167713a9127788eb2
keyhan <keyhmoham@gmail.com>
2025-07-20 22:26:12 +0330

    testing paths.

--- applet/docker/lorax/run.sh
+++ applet/docker/lorax/run.sh
@@ -1,5 +1,8 @@
 
-bash ~/.nvm/nvm.sh && node /app/chain-bridge/main.js &
+export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" &&
+    node /app/chain-bridge/main.js &
 
 lorax-launcher --json-output --model-id mistralai/Mistral-7B-Instruct-v0.1 --port 80 --max-batch-prefill-tokens 2048 --max-input-length 2048 &
 
