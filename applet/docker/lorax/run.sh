c0fed79d632a9a48faac71986d83f923077110ab
keyhan <keyhmoham@gmail.com>
2025-07-20 10:51:38 +0330

    trying to fix chain-bridge bug.

--- applet/docker/lorax/run.sh
+++ applet/docker/lorax/run.sh
@@ -1,5 +1,5 @@
 
-node /app/chain-bridge/main.js &
+source ~/.nvm/nvm.sh && node /app/chain-bridge/main.js &
 
 lorax-launcher --json-output --model-id mistralai/Mistral-7B-Instruct-v0.1 --port 80 --max-batch-prefill-tokens 2048 --max-input-length 2048 &
 
