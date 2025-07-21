ea12d9fc716f883f172d541817ecfa49822150cb
keyhan <keyhmoham@gmail.com>
2025-07-21 23:28:54 +0330

    added draggabl;e.

--- client/web/flutter_service_worker.js
+++ client/web/flutter_service_worker.js
@@ -4,12 +4,12 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "bb80592e6218ef4e7c23329ebb34367f",
+"main.dart.js": "f64d48f02f18abd5412778d63bd4683e",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"flutter_bootstrap.js": "86ff244c869595558ad8d1deefd8e9d8",
+"flutter_bootstrap.js": "bd91012f868621ca224ceb30fec941da",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "version.json": "7817fb855f01eee2f1b1f105aceb9364",
 "assets/NOTICES": "1916de5aa6b5f4b03f2a09af84957e32",
