c8f7df7c2648a551342ef76fabf8ca38a425a758
keyhan <keyhmoham@gmail.com>
2025-07-22 13:09:10 +0330

    trying to fix bug...

--- client/web/flutter_service_worker.js
+++ client/web/flutter_service_worker.js
@@ -4,12 +4,12 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "07cc2012aed669656d7ce382f94ff0e2",
+"main.dart.js": "4265399d01255fd0e3bd81d7e85200a3",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"flutter_bootstrap.js": "018642fb32a71006ba1aade486f58575",
+"flutter_bootstrap.js": "6e4ad000c9bbbc04930e3c11718312bd",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "version.json": "7817fb855f01eee2f1b1f105aceb9364",
 "assets/NOTICES": "1916de5aa6b5f4b03f2a09af84957e32",
