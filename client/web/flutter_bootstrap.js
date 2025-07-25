e921f99868f345cd370342fb2e6e5b58c39db7a0
keyhan <keyhmoham@gmail.com>
2025-07-25 21:02:58 +0330

    updated web app.

--- client/web/flutter_bootstrap.js
+++ client/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "343422867"
+    serviceWorkerVersion: "3714764630"
   }
 });
