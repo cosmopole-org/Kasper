b7b28ba8030cbd4838045e87ff5fe2ff13d7bc0a
keyhan <keyhmoham@gmail.com>
2025-07-26 18:04:14 +0330

    updated web app.

--- client/web/flutter_service_worker.js
+++ client/web/flutter_service_worker.js
@@ -4,12 +4,12 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "a4bef1dbd8114c689beb87b8ec60b9f4",
+"main.dart.js": "3c29166efa04793a532dbaae19e5f314",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"flutter_bootstrap.js": "b3c925accce3824ba26b5e2fb4ac0016",
+"flutter_bootstrap.js": "9fe7c7351da536cc8ce44a5002680639",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "version.json": "7817fb855f01eee2f1b1f105aceb9364",
 "assets/NOTICES": "14aaf95ab73b08032865232185ecd42d",
