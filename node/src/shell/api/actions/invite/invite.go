bec998c0499e2c8fa8d83db383c6cad5cc547913
keyhan <keyhmoham@gmail.com>
2025-08-11 16:21:19 +0330

    added more endpoints to invites

--- node/src/shell/api/actions/invite/invite.go
+++ node/src/shell/api/actions/invite/invite.go
@@ -1,6 +1,7 @@
 package actions_invite
 
 import (
+	"encoding/binary"
 	"errors"
 	"kasper/src/abstract/models/core"
 	"kasper/src/abstract/state"
@@ -11,6 +12,8 @@ import (
 	updates_points "kasper/src/shell/api/updates/points"
 	"kasper/src/shell/utils/future"
 	"log"
+	"strconv"
+	"time"
 )
 
 type Actions struct {
@@ -19,6 +22,16 @@ type Actions struct {
 
 func Install(a *Actions) error {
 	return nil
+}
+
+type UserInviteTemp struct {
+	model.User
+	Time int64 `json:"time"`
+}
+
+type PointInviteTemp struct {
+	model.Point
+	Time int64 `json:"time"`
 }
 
 // Create /invites/create check [ true true false ] access [ true false false false POST ]
@@ -33,6 +46,7 @@ func (a *Actions) Create(state state.IState, input inputsinvites.CreateInput) (a
 	point := model.Point{Id: state.Info().PointId()}.Pull(trx)
 	trx.PutLink("invite::"+point.Id+"::"+input.UserId, "true")
 	trx.PutLink("inviteto::"+input.UserId+"::"+point.Id, "true")
+	trx.PutLink("invitetime::"+point.Id+"::"+input.UserId, strconv.FormatInt(time.Now().UnixMilli(), 10))
 	future.Async(func() {
 		a.App.Tools().Signaler().SignalUser("invites/create", input.UserId, updatesinvites.Create{Point: point}, true)
 	}, false)
@@ -48,10 +62,17 @@ func (a *Actions) ListPointInvites(state state.IState, input inputsinvites.ListP
 	if !trx.HasObj("Point", state.Info().PointId()) {
 		return nil, errors.New("point not found")
 	}
-	users, err := model.User{}.List(trx, "invite::"+state.Info().PointId()+"::")
+	prefix := "invite::" + state.Info().PointId() + "::"
+	links, err := trx.GetLinksList(prefix, 0, 1000)
 	if err != nil {
 		log.Println(err)
 		return nil, err
+	}
+	users := []UserInviteTemp{}
+	for _, link := range links {
+		user := model.User{Id: link[len(prefix):]}.Pull(trx, true)
+		t, _ := strconv.ParseInt(trx.GetLink("invitetime::"+state.Info().PointId()+"::"+user.Id), 10, 64)
+		users = append(users, UserInviteTemp{User: user, Time: t})
 	}
 	return map[string]any{"users": users}, nil
 }
@@ -65,10 +86,17 @@ func (a *Actions) ListUserInvites(state state.IState, input inputsinvites.ListUs
 	if !trx.HasObj("Point", state.Info().PointId()) {
 		return nil, errors.New("point not found")
 	}
-	points, err := model.Point{}.List(trx, "invite::"+state.Info().UserId()+"::")
+	prefix := "inviteto::" + state.Info().UserId() + "::"
+	links, err := trx.GetLinksList(prefix, 0, 1000)
 	if err != nil {
 		log.Println(err)
 		return nil, err
+	}
+	points := []PointInviteTemp{}
+	for _, link := range links {
+		point := model.Point{Id: link[len(prefix):]}.Pull(trx, true)
+		t, _ := strconv.ParseInt(trx.GetLink("invitetime::"+point.Id+"::"+state.Info().UserId()), 10, 64)
+		points = append(points, PointInviteTemp{Point: point, Time: t})
 	}
 	return map[string]any{"points": points}, nil
 }
@@ -84,6 +112,7 @@ func (a *Actions) Cancel(state state.IState, input inputsinvites.CancelInput) (a
 	}
 	trx.DelKey("link::invite::" + state.Info().PointId() + "::" + input.UserId)
 	trx.DelKey("link::inviteto::" + input.UserId + "::" + state.Info().PointId())
+	trx.DelKey("link::invitetime::" + state.Info().PointId() + "::" + input.UserId)
 	point := model.Point{Id: state.Info().PointId()}.Pull(trx)
 	future.Async(func() {
 		a.App.Tools().Signaler().SignalUser("invites/cancel", input.UserId, updatesinvites.Cancel{Point: point}, true)
@@ -99,7 +128,9 @@ func (a *Actions) Accept(state state.IState, input inputsinvites.AcceptInput) (a
 	}
 	trx.DelKey("link::invite::" + input.PointId + "::" + state.Info().UserId())
 	trx.DelKey("link::inviteto::" + state.Info().UserId() + "::" + input.PointId)
+	trx.DelKey("link::invitetime::" + input.PointId + "::" + state.Info().UserId())
 	trx.PutLink("member::"+input.PointId+"::"+state.Info().UserId(), "true")
+	trx.PutLink("memberof::"+state.Info().UserId()+"::"+input.PointId, "true")
 	a.App.Tools().Signaler().JoinGroup(input.PointId, state.Info().UserId())
 	admins, err := trx.GetLinksList("admin::"+input.PointId+"::", -1, -1)
 	if err != nil {
@@ -126,6 +157,7 @@ func (a *Actions) Decline(state state.IState, input inputsinvites.DeclineInput) 
 	}
 	trx.DelKey("link::invite::" + input.PointId + "::" + state.Info().UserId())
 	trx.DelKey("link::inviteto::" + state.Info().UserId() + "::" + input.PointId)
+	trx.DelKey("link::invitetime::" + input.PointId + "::" + state.Info().UserId())
 	admins, err := trx.GetLinksList("admin::"+input.PointId+"::", -1, -1)
 	if err != nil {
 		log.Println(err)
