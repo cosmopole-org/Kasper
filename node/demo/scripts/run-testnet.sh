aa6271acd384b19286e868f2028df2d989c27083
keyhan <keyhmoham@gmail.com>
2025-08-14 23:18:57 +0330

    fixed address

--- node/demo/scripts/run-testnet.sh
+++ node/demo/scripts/run-testnet.sh
@@ -5,8 +5,7 @@ docker create -p 8077:8079 -p 8078:8080 -p 8079:8081 -p 8080:8082 --name=node1 \
     --net=host \
     -v /var/run/docker.sock:/var/run/docker.sock \
     --mount type=bind,source=/home/keyhan/certs,target=/app/certs \
-    --mount type=bind,source=/home/keyhan/basedb,target=/app/basedb \
-    --mount type=bind,source=/home/keyhan/data1,target=/app/storage \
+    --mount type=bind,source=/home/keyhan/data,target=/app/storage \
     --privileged \
     --device /dev/kvm \
     -v /lib/modules:/lib/modules \
