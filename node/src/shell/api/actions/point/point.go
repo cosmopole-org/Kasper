d7a24b01b0b75ede4b719ace307a0872d6a1b225
keyhan <keyhmoham@gmail.com>
2025-08-18 17:37:24 +0330

    fixed bug.

--- node/src/shell/api/actions/point/point.go
+++ node/src/shell/api/actions/point/point.go
@@ -473,7 +473,7 @@ func (a *Actions) Create(state state.IState, input inputs_points.CreateInput) (a
 		a.App.Tools().Signaler().JoinGroup(point.Id, memberId)
 	}
 	point = point.Pull(trx, true)
-	a.App.Tools().Signaler().SignalGroup("points/create", point.Id, updates_points.Delete{Point: point}, true, []string{})
+	a.App.Tools().Signaler().SignalGroup("points/create", point.Id, updates_points.Delete{Point: point}, true, []string{state.Info().UserId()})
 	return outputs_points.CreateOutput{Point: outputs_points.AdminPoiint{Point: point, Admin: true}}, nil
 }
 
