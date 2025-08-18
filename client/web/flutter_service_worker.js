4b8b8d343dab23403d2a6afbf0d18a5fabf35b36
keyhan <keyhmoham@gmail.com>
2025-08-18 18:13:35 +0330

    updated web app

--- client/web/flutter_service_worker.js
+++ client/web/flutter_service_worker.js
@@ -4,13 +4,13 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "2049b7615e0aa07b797a019a0420db0c",
+"main.dart.js": "e582eae1c464f1568378420542fb6204",
 "oauthsuccess.html": "2bc714899f1d0c0e8ae3e811e8490826",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"flutter_bootstrap.js": "ecc512514d60f6e9ccb89637b1a8ac1c",
+"flutter_bootstrap.js": "66f5d08e5b1b57a8b88524fc2186d857",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "version.json": "7817fb855f01eee2f1b1f105aceb9364",
 "assets/NOTICES": "ee77504f084f485dad05babbad910218",
