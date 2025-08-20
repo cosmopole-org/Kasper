c4d28229061668b3ee369cb90310d8cdcba83db7
keyhan <keyhmoham@gmail.com>
2025-08-20 09:20:16 +0330

    fixed bug.

--- node/src/shell/api/actions/user/user.go
+++ node/src/shell/api/actions/user/user.go
@@ -292,7 +292,7 @@ func (a *Actions) Delete(state state.IState, input inputsusers.DeleteInput) (any
 	trx.DelKey("link::UserPrivateKey::"+user.Id)
 	trx.DelKey("link::UserEmailToId::"+email)
 	trx.DelKey("link::UserIdToEmail::"+user.Id)
-	trx.DelIndex("User", "username", "id", user.Username+"@"+a.App.Id())
+	trx.DelIndex("User", "username", "id", user.Username)
 	user.Username = "deleted_user"
 	user.PublicKey = ""
 	user.Balance = 0
