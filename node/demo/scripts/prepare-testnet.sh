ff389008736c76f3f1a214631a85cdbee00687e8
keyhan <keyhmoham@gmail.com>
2025-08-27 17:05:32 +0330

    fixed

--- node/demo/scripts/prepare-testnet.sh
+++ node/demo/scripts/prepare-testnet.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-docker network create \
-  --driver bridge \
-  --subnet 172.77.0.0/16 \
-  --gateway 172.77.0.1 \
-  kasper
+docker network create kasper
 
-docker run --name=logsdb --net=kasper --ip=172.77.0.9 -p 9042:9042 -e MAX_HEAP_SIZE=512M -e HEAP_NEWSIZE=100M cassandra
+docker run --name=logsdb --net=kasper --ip=172.18.0.9 -p 9042:9042 -e MAX_HEAP_SIZE=512M -e HEAP_NEWSIZE=100M cassandra
