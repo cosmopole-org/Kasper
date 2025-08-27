c21330f1b3e47b00f3484a10439afbf147a5adbc
keyhan <keyhmoham@gmail.com>
2025-08-27 17:10:35 +0330

    trying to fix...

--- node/demo/scripts/prepare-testnet.sh
+++ node/demo/scripts/prepare-testnet.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker network create kasper
+docker network create \
+  --driver bridge \
+  --subnet 10.10.0.0/16 \
+  --gateway 10.10.0.1 \
+  kasper
 
-docker run --name=logsdb --net=kasper --ip=172.18.0.9 -p 9042:9042 -e MAX_HEAP_SIZE=512M -e HEAP_NEWSIZE=100M cassandra
+docker run --name=logsdb --net=kasper --ip=10.10.0.9 -p 9042:9042 -e MAX_HEAP_SIZE=512M -e HEAP_NEWSIZE=100M cassandra
