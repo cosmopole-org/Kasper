666768295de0730b2f0c5a3578785fd92830b056
keyhan <keyhmoham@gmail.com>
2025-08-30 01:05:52 +0330

    fix

--- node/src/shell/api/actions/storage/storage.go
+++ node/src/shell/api/actions/storage/storage.go
@@ -357,7 +357,7 @@ func Install(a *Actions) error {
 				http.Error(w, "can't access point", http.StatusForbidden)
 				return
 			}
-			url := fmt.Sprintf("%s://%s%s", "127.0.0.1", "localhost:80", "/"+strings.Join(strings.Split(input.MachineId, "@"), "_")+"/stream/")
+			url := fmt.Sprintf("%s://%s%s", "http", "127.0.0.1", "/"+strings.Join(strings.Split(input.MachineId, "@"), "_")+"/stream/")
 			proxyReq, err := http.NewRequest("GET", url, bytes.NewReader([]byte("{}")))
 			if err != nil {
 				log.Println(err)
