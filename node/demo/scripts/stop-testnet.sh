73e21d98aae82cd6f30d07b880c07e9997f6a19c
keyhan <keyhmoham@gmail.com>
2025-08-30 20:58:50 +0330

    fixed

--- node/demo/scripts/stop-testnet.sh
+++ node/demo/scripts/stop-testnet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker ps -f name=logsdb -f name=node -aq | xargs docker stop | xargs docker rm -f 
+docker ps -f kasper-proxy -f name=logsdb -f name=node -aq | xargs docker stop | xargs docker rm -f 
 
 docker network rm kasper
