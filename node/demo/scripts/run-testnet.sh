6bf57a6ac63c95d41e7f642b144e26aa8365af39
keyhan <keyhmoham@gmail.com>
2025-09-04 10:53:09 +0330

    fixed config.

--- node/demo/scripts/run-testnet.sh
+++ node/demo/scripts/run-testnet.sh
@@ -13,6 +13,7 @@ docker create -p 3000:3000 -p 8074:8074 -p 8076:8076 -p 8077:8077 -p 8078:8078 -
     --net=kasper \
     --ip=10.10.0.3 \
     -v /var/run/docker.sock:/var/run/docker.sock \
+    --mount type=bind,source=/root/.babble,target=/root/.babble \
     --mount type=bind,source=/home/keyhan/certs,target=/app/certs \
     --mount type=bind,source=/home/keyhan/data,target=/app/storage \
     --privileged \
