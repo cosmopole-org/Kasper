a7ed27aed11d476ae5b2f9580c7439229e29a938
keyhan <keyhmoham@gmail.com>
2025-09-04 11:18:57 +0330

    fixed

--- node/demo/scripts/join.sh
+++ node/demo/scripts/join.sh
@@ -10,29 +10,15 @@ dest=$DEST/node$N
 # Create new key-pair and place it in new conf directory
 mkdir -p $dest
 echo "Generating key pair for node$N"
-docker run  \
-    -u $(id -u) \
-    -v $dest:/.babble \
-    --rm kasper:latest keygen 
-
-# copy signal TLS certificate
-cp $PWD/../src/net/signal/wamp/test_data/cert.pem $dest/cert.pem
+go run ../..//keygen/keygen.go
 
 # get genesis.peers.json
 echo "Fetching peers.genesis.json from node1"
-curl -s http://172.77.5.1:80/genesispeers > $dest/peers.genesis.json
+curl -s http://165.232.32.106:80/genesispeers > $dest/peers.genesis.json
 
 # get up-to-date peers.json
 echo "Fetching peers.json from node1"
 curl -s http://172.77.5.1:80/peers > $dest/peers.json
-
-# start the new node
-docker run -d --name=client$N --net=babblenet --ip=172.77.10.$N -it mosaicnetworks/dummy:latest \
-    --name="client $N" \
-    --client-listen="172.77.10.$N:1339" \
-    --proxy-connect="172.77.5.$N:1338" \
-    --discard \
-    --log="debug" 
 
 docker create --name=node$N --net=babblenet --ip=172.77.5.$N kasper:latest run \
     --heartbeat=100ms \
