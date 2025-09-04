f8dbbca47b11f3ea1595dd4360b9c46be2d877a4
keyhan <keyhmoham@gmail.com>
2025-09-04 11:41:38 +0330

    fixed bug.

--- node/demo/scripts/join.sh
+++ node/demo/scripts/join.sh
@@ -14,11 +14,11 @@ go run ../..//keygen/keygen.go
 
 # get genesis.peers.json
 echo "Fetching peers.genesis.json from node1"
-curl -s http://165.232.32.106:80/genesispeers > $dest/peers.genesis.json
+curl -s http://165.232.32.106:8079/genesispeers > $dest/peers.genesis.json
 
 # get up-to-date peers.json
 echo "Fetching peers.json from node1"
-curl -s http://172.77.5.1:80/peers > $dest/peers.json
+curl -s http://165.232.32.106:8079/peers > $dest/peers.json
 
 cp $dest /root/.babble
 
