f1f6f3a2edbff94a9ea8d038c184fcbc13beb93a
keyhan <keyhmoham@gmail.com>
2025-09-13 13:27:10 +0330

    fixed bug.

--- node/scripts/run-questdb.sh
+++ node/scripts/run-questdb.sh
@@ -7,7 +7,7 @@ export QDB_PG_ENABLED=true
 export QDB_PG_PORT=5432
 export QDB_PG_BIND=0.0.0.0
 
-echo "pg.enabled=true\npg.port=5432\npg.bind=0.0.0.0">$QUESTDB_DIR/conf/server.conf
+echo "pg.enabled=true\npg.port=5432\npg.bind=0.0.0.0">>/root/.questdb/conf/server.conf
 
 # Start QuestDB in background
 echo "Starting QuestDB..."
