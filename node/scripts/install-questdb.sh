80f61d1fbfc9250b8fac8a10cc5dd7c9924c8538
Keyhan Mohammadi <theprogrammermachine@gmail.com>
2025-10-15 22:48:36 +0330

    fixed bug.

--- node/scripts/install-questdb.sh
+++ node/scripts/install-questdb.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Variables
-QUESTDB_VERSION="9.0.3"
+QUESTDB_VERSION="9.1.0"
 QUESTDB_DIR="/app/questdb"
 QUESTDB_PORT=9000
 PG_PORT=5432
@@ -10,7 +10,7 @@ PG_PORT=5432
 # Download QuestDB if not exists
 if [ ! -d "$QUESTDB_DIR" ]; then
     echo "Downloading QuestDB $QUESTDB_VERSION..."
-    curl -L https://github.com/questdb/questdb/releases/download/9.1.0/questdb-9.1.0-no-jre-bin.tar.gz -o /tmp/questdb.tar.gz
+    curl -L https://github.com/questdb/questdb/releases/download/9.1.0/questdb-$QUESTDB_VERSION-no-jre-bin.tar.gz -o /tmp/questdb.tar.gz
     tar -xzf /tmp/questdb.tar.gz -C /app
-    mv /app/questdb-$QUESTDB_VERSION-rt-linux-x86-64 $QUESTDB_DIR
+    mv /app/questdb-$QUESTDB_VERSION-no-jre-bin $QUESTDB_DIR
 fi
